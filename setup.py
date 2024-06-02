a797a62de0bbb9247213ed44f35e43b6531f7116
Abhishek Tiwari <abhishek.tiwari.nitrr@gmail.com>
2024-06-02 20:40:32 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Abhishek Tiwari",
+    author_email="abhitiwari20182@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
